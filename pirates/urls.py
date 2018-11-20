611bfee849370b41c28445dde16fab0585d3655c
Lucas Corrêa <lucashuati@hotmail.com>
2018-11-20 08:21:02 -0200

    Exercice 3 Done

--- pirates/urls.py
+++ pirates/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ListaTesourosView.as_view()),
+    path('', views.ListaTesourosView.as_view(), name='list'),
+    path('new', views.SalvarTesouroView.as_view(), name='new'),
 ]
--- pirates/views.py
+++ pirates/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.db.models import F, ExpressionWrapper, DecimalField, Sum
-from . import models
+from . import models, forms
+from django.contrib import messages
 
 
 class ListaTesourosView(View):
@@ -22,5 +23,31 @@ class ListaTesourosView(View):
             context=dict(
                 lista_tesouros=lista_tesouros,
                 total_geral=lista_tesouros.aggregate(Sum('total'))['total__sum']
+            )
+        )
+
+
+class SalvarTesouroView(View):
+    def get(self, request):
+        return render(
+            request,
+            template_name='salvar_tesouro.html',
+            context=dict(
+                form=forms.TesouroForm
+            )
+        )
+
+    def post(self, request):
+        form = forms.TesouroForm(request.POST, request.FILES)
+        if form.is_valid():
+            messages.add_message(request, messages.SUCCESS, 'Tesouro criado com sucesso!')
+            form.save()
+            return redirect('list')
+        messages.add_message(request, messages.ERROR, 'Ocorreu um erro ao criar o tesouro!')
+        return render(
+            request,
+            template_name='salvar_tesouro.html',
+            context=dict(
+                form=form
             )
         )
